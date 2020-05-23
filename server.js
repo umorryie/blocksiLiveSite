d512c73486b0cf192fe6d7e63543fae0ead9476f
MatejJs <pesjak.matej@gmail.com>
2020-05-23 18:20:19 +0200

    added to production

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const multer = require("multer");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const router = require("./routes/routes");
+const path = require("path");
 const port = process.env.PORT || 5000;
 const uri = require("./mongoDb/key").uri;
 require("./mongoDb/mongoDb");
@@ -18,6 +19,13 @@ app.use(cors());
 app.use(cookieParser());
 
 app.use(router);
+
+if (process.env.NODE_END === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
